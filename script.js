024171b2a59c1fe3ea0f57e83ee9035e17ab7689
ZhengChen <zhengchen1025@gmail.com>
2024-04-24 17:25:46 +0800

    finished

--- script.js
+++ script.js
@@ -12,64 +12,83 @@ function getComputerChoice() {
   }
 }
 
-function promptUserChoice() {
-  let choice = prompt(
-    "What's your choice: rock, paper or scissors?"
-  ).toLowerCase();
-  if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
-    promptUserChoice();
-  } else {
-    console.log(`you picked ${choice}`);
-    return choice;
-  }
-}
-
 function singleRound(playerSelection, computerSelection) {
   if (
-    (playerSelection == "rock" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "rock")
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
   ) {
-    console.log(`"You lose! ${computerSelection} beats ${playerSelection}"`);
+    gameInfoDiv.innerHTML += `You lose! ${computerSelection} beats ${playerSelection}`;
     return "You lose";
   } else if (
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "paper")
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    console.log(`"You win! ${playerSelection} beats ${computerSelection}"`);
+    gameInfoDiv.innerHTML += `You win! ${playerSelection} beats ${computerSelection}`;
     return "You win";
-  } else if (playerSelection == computerSelection) {
-    console.log(`"You both picked ${computerSelection}, tie!"`);
+  } else if (playerSelection === computerSelection) {
+    gameInfoDiv.innerHTML += `You both picked ${computerSelection}, tie!`;
     return "Tie";
   }
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
-function playGame() {
-  console.log("game starts");
-
-  for (let i = 0; i < 5; i++) {
-    console.log(`round ${i + 1}`);
-
-    let playerSelection = promptUserChoice();
-
-    let computerSelection = getComputerChoice();
-
-    let result = singleRound(playerSelection, computerSelection);
-    console.log(`result: ${result}`);
-
-    if (result === "You win") {
-      playerScore += 1;
-    } else if (result === "You lose") {
-      computerScore += 1;
-    } else {
-      console.log("Tie");
-    }
-    console.log(`Your score: ${playerScore}, Computer score: ${computerScore}`);
+function calcScore() {
+  if (result === "You win") {
+    playerScore += 1;
+  } else if (result === "You lose") {
+    computerScore += 1;
+  } else {
+    console.log("Tie");
   }
+  gameInfoDiv.innerHTML += `<p>Your score: ${playerScore}, Computer score: ${computerScore}</p>`;
+  i++;
 }
 
-playGame();
+const gameInfoDiv = document.getElementById("gameInfo");
+let playerScore = 0;
+let computerScore = 0;
+let i = 0;
+let playerSelection;
+let computerSelection = "";
+let result;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const rock = document.getElementById("rock");
+  rock.addEventListener("click", () => {
+    playerSelection = "rock";
+    gameInfoDiv.innerHTML += `<p>player picked ${playerSelection}</p>`;
+    result = singleRound(playerSelection, computerSelection);
+    calcScore();
+    reset();
+  });
+
+  const paper = document.getElementById("paper");
+  paper.addEventListener("click", () => {
+    playerSelection = "paper";
+    gameInfoDiv.innerHTML += `<p>player picked ${playerSelection}</p>`;
+    result = singleRound(playerSelection, computerSelection);
+    calcScore();
+    reset();
+  });
+
+  const scissors = document.getElementById("scissors");
+  scissors.addEventListener("click", () => {
+    playerSelection = "scissors";
+    gameInfoDiv.innerHTML += `<p>player picked ${playerSelection}</p>`;
+    result = singleRound(playerSelection, computerSelection);
+    calcScore();
+    reset();
+  });
+});
+
+function reset() {
+  gameInfoDiv.innerHTML += `<p>round ${i + 1}</p>`;
+  computerSelection = getComputerChoice();
+  gameInfoDiv.innerHTML += `<p>please pick one</p>`;
+}
+
+gameInfoDiv.innerHTML += "<p>game starts</p>";
+gameInfoDiv.innerHTML += `<p>round ${i + 1}</p>`;
+computerSelection = getComputerChoice();
+gameInfoDiv.innerHTML += `<p>please pick one</p>`;
